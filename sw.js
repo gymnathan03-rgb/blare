dcb729e542178df8fa07db4bec962c7fda0eb6c4
gymnathan03-rgb <gymnathan03@gmail.com>
2026-09-14 13:00:47 -0700

    Bump cache version to v6

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "blare-v5";
+const CACHE_NAME = "blare-v6";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=2",
-  "./app.js?v=2",
+  "./style.css?v=3",
+  "./app.js?v=3",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
